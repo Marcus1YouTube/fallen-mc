debebaa669b90e0d3b60eff9db25f52e9ab90f74
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2022-06-04 10:47:52 +0200

    home page updated (next up: announcements)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Drawer } from '@mantine/core'
 
 export default function Home() {
   useEffect(() => {
     document.title = 'Fallen MC - Főoldal'
   }, [])
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div className='Home'>
-        <h1>Hellóka gyerekek</h1>
+        <h1>Üdvözlünk a főoldalon!</h1>
+        <p>Itt lent találod az oldalakhoz a linkeket:</p>
+        <Button variant='outline' onClick={() => setOpen(true)}>Linktár megnyitása</Button>
+        <Drawer opened={open} onClose={() => setOpen(false)} title="Linktár" position='bottom' className="link-storage" transition="pop">
+          <div className='links'>
+            <Link to='/bejelentesek'><Button variant='outline'>Bejelentések</Button></Link>
+          </div>
+          
+
+        </Drawer>
     </div>
   )
 }

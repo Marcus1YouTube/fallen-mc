d3f9382a8ca8a8601e6089bcbaf9b5ecf6eeda12
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2022-06-04 10:21:24 +0200

    mantine integráció

--- src/pages/Hiba.js
+++ src/pages/Hiba.js
@@ -1,5 +1,6 @@
 import {useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '@mantine/core'
 
 export default function Error() {
   useEffect(() => {
@@ -8,9 +9,9 @@ export default function Error() {
   return (
     <div className='Hiba'>
         <h1>Hiba történt: Nem találtuk ezt a címet!</h1>
-        <img src='err.png' placeholder='Ez egy majom, amit a YouTube-ról loptam xd' title='Ez egy majom, amit a YouTube-ról loptam xd'></img>
-        <p>Micsoda tragédia! Hát ez hogy történhetett? <br /> Mindegy, ez a szerencsés gomb segít visszatalálni a főoldalra! <big>🙂</big></p>
-        <Link to='/'><button>Vissza a főoldalra</button></Link>
+        <img src='err.png' placeholder='Ez egy majom, amit a YouTube-ról loptam xd' title='Ez egy majom, amit a YouTube-ról loptam xd' alt='Ez egy majom, amit a YouTube-ról loptam xd'></img>
+        <p>Micsoda tragédia! Hát ez hogy történhetett? <br /> Mindegy, ez a gomb segít visszatalálni a főoldalra! <big>🙂</big></p>
+        <Link to='/'><Button variant='outline'>Vissza a főoldalra</Button></Link>
         
     </div>
   )

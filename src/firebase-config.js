fb7f655bcd813629723427fee167e790912f85e1
Marcus1YouTube <75576363+Marcus1YouTube@users.noreply.github.com>
2022-06-06 10:28:42 +0200

    firebase creds hidden

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNBL-R7StdTM0t5w7RC4CTU_w2MRAIjn4",
-  authDomain: "fallen-mc.firebaseapp.com",
-  projectId: "fallen-mc",
-  storageBucket: "fallen-mc.appspot.com",
-  messagingSenderId: "60894106791",
-  appId: "1:60894106791:web:b1c95f551549834acf3782",
-  measurementId: "G-9P2DK9G6QF",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
